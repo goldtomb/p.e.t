e24d3b89c083cc1986e8dd58da4f9aaaadffd96b
goldtomb <gt.morph@gmail.com>
2019-12-30 20:52:27 -0800

    commit

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const stripePublicKey = process.env.STRIPE_PUBLIC_KEY;
 const express = require('express');
 const stripe = require('stripe')(stripeSecretKey);
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 
 const app = express();
@@ -18,9 +19,20 @@ const port = process.env.port|| 3000;
 app.set('view engine', 'ejs');
 app.use(express.static(`${__dirname}/public`));
 
-app.use(bodyParser.json(), extended);
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.json({extended: false}),);
+app.use(bodyParser.urlencoded({extended: false}));
 
+app.get('/classes', (req, res) => {
+    fs.readFile('./models/classes.json', (req, res) => {
+        if(error) {
+            res.status(500).end()
+        } else {
+            res.render('classes.ejs', {
+                items: JSON.parse(data)
+            })
+        }
+    })
+});
 
 app.listen(port, () => {
     console.log(`Server started on port: ${port}.`);
