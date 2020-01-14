a1272273bffd9bf412e5841db0947dc621845f60
goldtomb <gt.morph@gmail.com>
2020-01-13 18:52:53 -0800

    yuhh

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const fs = require('fs');
 
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 /** EJS MIDDLEWARE */
 app.set('view engine', 'ejs'); +
